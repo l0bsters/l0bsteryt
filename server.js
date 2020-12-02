34ee2ccf9255c879f068281d18e8eb879c942661
underd0g1 <hide4@comcast.net>
2020-12-01 23:12:44 -0500

    added api

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ var app = express();
 var ytcall = require('youtube-api-v3-search');
 var ytfunction = require('./lib/events.js');
 
-var song = process.env.input;
-console.log(song);
-ytfunction.ytresults(song);
+
+if (process.env.site == 'yt'){
+
+	//var song = process.env.input;
+	var song = 'https://www.youtube.com/watch?v=D74gejJX3QU&list=RDD74gejJX3QU&start_radio=1'
+	//console.log(song);
+	//ytfunction.ytresults(song);
+	ytfunction.ytdownload(song);
+
+}else if (process.env.site == 'ph'){
+	console.log('nothing found');
+}
 
 
