a57da05bf7f66b99dac6fb8553f6635832f0bbb4
l0bsterz <hide4653@gmail.com>
2021-01-07 23:36:16 -0500

    added exec

--- server.js
+++ server.js
@@ -2,18 +2,26 @@ var express = require('express');
 var app = express();
 var ytcall = require('youtube-api-v3-search');
 var ytfunction = require('./lib/events.js');
+var prompt = require('prompt');
 
 
-if (process.env.site == 'yt'){
+
+prompt.get(['song'], function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('Command-line input received:');
+    console.log('  S0ng: ' + result.song);
+    ytfunction.ytresults(result.song)
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
 
 	//var song = process.env.input;
-	var song = 'https://www.youtube.com/watch?v=D74gejJX3QU&list=RDD74gejJX3QU&start_radio=1'
+	//var song = 'https://www.youtube.com/watch?v=D74gejJX3QU&list=RDD74gejJX3QU&start_radio=1'
 	//console.log(song);
 	//ytfunction.ytresults(song);
-	ytfunction.ytdownload(song);
-
-}else if (process.env.site == 'ph'){
-	console.log('nothing found');
-}
+	//ytfunction.ytdownload(song);
 
 
