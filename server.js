a10b302f8331739317a1ad1a8ae06c87195462ff
underd0g1 <hide4@comcast.net>
2021-01-29 11:16:59 -0500

    added fzf base

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ var prompt = require('prompt');
 
 prompt.get(['song'], function (err, result) {
     if (err) { return onErr(err); }
-    console.log('Command-line input received:');
-    console.log('  S0ng: ' + result.song);
-    ytfunction.ytresults(result.song)
+    if(result.song){
+        console.log("there was input" , result.song);
+        ytfunction.ytresults(result.song);
+    }else{
+        console.log('no input yet');
+    }
 });
 
 function onErr(err) {
@@ -18,10 +21,10 @@ function onErr(err) {
     return 1;
 }
 
-	//var song = process.env.input;
-	//var song = 'https://www.youtube.com/watch?v=D74gejJX3QU&list=RDD74gejJX3QU&start_radio=1'
-	//console.log(song);
-	//ytfunction.ytresults(song);
-	//ytfunction.ytdownload(song);
+
+
+
+
+
 
 
